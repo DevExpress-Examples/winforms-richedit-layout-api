2ff9eb675f32fe8daf437ede470a171af76ad92e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/LayoutApiSimpleExample/Form1.cs
@@ -0,0 +1,78 @@
+﻿using DevExpress.XtraRichEdit;
+using DevExpress.XtraRichEdit.API.Layout;
+using DevExpress.XtraRichEdit.API.Native;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LayoutApiSimpleExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            richEditControl1.LoadDocument("Grimm.docx");
+            richEditControl1.DocumentLayout.DocumentFormatted += DocumentLayout_DocumentFormatted;
+        }
+        #region #DocumentFormatted
+        private void DocumentLayout_DocumentFormatted(object sender, EventArgs e)
+        {          
+
+            richEditControl1.BeginInvoke(new Action(() =>
+            {
+                int pageCount = richEditControl1.DocumentLayout.GetFormattedPageCount();
+                for (int i = 0; i < pageCount; i++)
+                {
+                    MyDocumentLayoutVisitor visitor = new MyDocumentLayoutVisitor();
+                    visitor.Visit(richEditControl1.DocumentLayout.GetPage(i));
+                }
+            }));
+        }
+        #endregion #DocumentFormatted
+
+        private void btnPageInfo_Click(object sender, EventArgs e)
+        {
+            #region #GetVisiblePages
+            List<PageLayoutInfo> pages = richEditControl1.ActiveView.GetVisiblePageLayoutInfos();
+            DocumentRange visibleRange = richEditControl1.ActiveView.GetVisiblePagesRange();
+
+            Pen redPen = new Pen(Color.Red, 1);
+            Graphics gr = richEditControl1.CreateGraphics();
+            gr.DrawRectangle(redPen,pages[0].Bounds);
+
+            string s = String.Format("{0} page(s) displayed.\r\n" +
+                "From page {1} which starts at document position {2}.\r\n" +
+                "To page {3} which ends at document position {4}.\r\n" + 
+                "First visible page is outlined with red.",
+            pages.Count, pages[0].PageIndex, visibleRange.Start, pages[pages.Count - 1].PageIndex, visibleRange.End);
+            #endregion  #GetVisiblePages
+            lblPageInfo.Text = s;
+        }
+
+        private void trackBarControl1_EditValueChanged(object sender, EventArgs e)
+        {
+            richEditControl1.ActiveView.ZoomFactor = trackBarControl1.Value / 100f;
+        }
+
+        private void btnCheckLayout_Click(object sender, EventArgs e)
+        {
+            #region #CheckLayout
+            DevExpress.XtraRichEdit.API.Native.Table table = richEditControl1.Document.Tables.First;
+            if (table != null)
+            {
+                // Obtain the layout element related to the table.
+                LayoutTable ltable = richEditControl1.DocumentLayout.GetElement<LayoutTable>(table.Range.Start);
+                // Obtain zero-based page index of the page containing the layout element.
+                int pageIndex = this.richEditControl1.DocumentLayout.GetPageIndex(ltable);
+                // Check whether the layout element is located at the second page.
+                string s = "Layout verified.";
+                if (pageIndex != 1)
+                    s = "The first table is not on the page 2. Review pagination.";
+                MessageBox.Show(s, "Check Layout", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            #endregion #CheckLayout
+        }
+    }
+}
---
+++ VB/LayoutApiSimpleExample/Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿Namespace LayoutApiSimpleExample
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Dim trackBarLabel1 As New DevExpress.XtraEditors.Repository.TrackBarLabel()
+			Dim trackBarLabel2 As New DevExpress.XtraEditors.Repository.TrackBarLabel()
+			Dim trackBarLabel3 As New DevExpress.XtraEditors.Repository.TrackBarLabel()
+			Dim trackBarLabel4 As New DevExpress.XtraEditors.Repository.TrackBarLabel()
+			Dim trackBarLabel5 As New DevExpress.XtraEditors.Repository.TrackBarLabel()
+			Dim trackBarLabel6 As New DevExpress.XtraEditors.Repository.TrackBarLabel()
+			Dim trackBarLabel7 As New DevExpress.XtraEditors.Repository.TrackBarLabel()
+			Dim trackBarLabel8 As New DevExpress.XtraEditors.Repository.TrackBarLabel()
+			Dim trackBarLabel9 As New DevExpress.XtraEditors.Repository.TrackBarLabel()
+			Dim trackBarLabel10 As New DevExpress.XtraEditors.Repository.TrackBarLabel()
+			Dim trackBarLabel11 As New DevExpress.XtraEditors.Repository.TrackBarLabel()
+			Me.richEditControl1 = New DevExpress.XtraRichEdit.RichEditControl()
+			Me.panelControl1 = New DevExpress.XtraEditors.PanelControl()
+			Me.trackBarControl1 = New DevExpress.XtraEditors.TrackBarControl()
+			Me.btnPageInfo = New DevExpress.XtraEditors.SimpleButton()
+			Me.lblPageInfo = New DevExpress.XtraEditors.LabelControl()
+			Me.btnCheckLayout = New DevExpress.XtraEditors.SimpleButton()
+			DirectCast(Me.panelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.panelControl1.SuspendLayout()
+			DirectCast(Me.trackBarControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			DirectCast(Me.trackBarControl1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' richEditControl1
+			' 
+			Me.richEditControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.richEditControl1.EnableToolTips = True
+			Me.richEditControl1.Location = New System.Drawing.Point(0, 0)
+			Me.richEditControl1.Name = "richEditControl1"
+			Me.richEditControl1.Options.Bookmarks.AllowNameResolution = False
+			Me.richEditControl1.Options.Printing.EnableCommentBackgroundOnPrint = False
+			Me.richEditControl1.Options.Printing.EnableCommentFillOnPrint = False
+			Me.richEditControl1.Size = New System.Drawing.Size(784, 504)
+			Me.richEditControl1.TabIndex = 0
+			Me.richEditControl1.Text = "richEditControl1"
+			' 
+			' panelControl1
+			' 
+			Me.panelControl1.Controls.Add(Me.btnCheckLayout)
+			Me.panelControl1.Controls.Add(Me.trackBarControl1)
+			Me.panelControl1.Controls.Add(Me.btnPageInfo)
+			Me.panelControl1.Controls.Add(Me.lblPageInfo)
+			Me.panelControl1.Dock = System.Windows.Forms.DockStyle.Bottom
+			Me.panelControl1.Location = New System.Drawing.Point(0, 504)
+			Me.panelControl1.Name = "panelControl1"
+			Me.panelControl1.Size = New System.Drawing.Size(784, 80)
+			Me.panelControl1.TabIndex = 1
+			' 
+			' trackBarControl1
+			' 
+			Me.trackBarControl1.EditValue = 100
+			Me.trackBarControl1.Location = New System.Drawing.Point(502, 6)
+			Me.trackBarControl1.Name = "trackBarControl1"
+			Me.trackBarControl1.Properties.LabelAppearance.Options.UseTextOptions = True
+			Me.trackBarControl1.Properties.LabelAppearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center
+			trackBarLabel1.Label = "0"
+			trackBarLabel2.Label = "100"
+			trackBarLabel2.Value = 100
+			trackBarLabel3.Label = "200"
+			trackBarLabel3.Value = 200
+			trackBarLabel4.Label = "300"
+			trackBarLabel4.Value = 300
+			trackBarLabel5.Label = "400"
+			trackBarLabel5.Value = 400
+			trackBarLabel6.Label = "500"
+			trackBarLabel6.Value = 500
+			trackBarLabel7.Label = "600"
+			trackBarLabel7.Value = 600
+			trackBarLabel8.Label = "700"
+			trackBarLabel8.Value = 700
+			trackBarLabel9.Label = "800"
+			trackBarLabel9.Value = 800
+			trackBarLabel10.Label = "900"
+			trackBarLabel10.Value = 900
+			trackBarLabel11.Label = "1000"
+			trackBarLabel11.Value = 1000
+			Me.trackBarControl1.Properties.Labels.AddRange(New DevExpress.XtraEditors.Repository.TrackBarLabel() { trackBarLabel1, trackBarLabel2, trackBarLabel3, trackBarLabel4, trackBarLabel5, trackBarLabel6, trackBarLabel7, trackBarLabel8, trackBarLabel9, trackBarLabel10, trackBarLabel11})
+			Me.trackBarControl1.Properties.Maximum = 300
+			Me.trackBarControl1.Properties.ShowLabels = True
+			Me.trackBarControl1.Properties.SmallChange = 10
+			Me.trackBarControl1.Properties.TickFrequency = 10
+			Me.trackBarControl1.Size = New System.Drawing.Size(270, 72)
+			Me.trackBarControl1.TabIndex = 2
+			Me.trackBarControl1.Value = 100
+'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
+'ORIGINAL LINE: this.trackBarControl1.EditValueChanged += new System.EventHandler(this.trackBarControl1_EditValueChanged);
+			' 
+			' btnPageInfo
+			' 
+			Me.btnPageInfo.Location = New System.Drawing.Point(12, 9)
+			Me.btnPageInfo.Name = "btnPageInfo"
+			Me.btnPageInfo.Size = New System.Drawing.Size(130, 23)
+			Me.btnPageInfo.TabIndex = 1
+			Me.btnPageInfo.Text = "Get Page Information"
+'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
+'ORIGINAL LINE: this.btnPageInfo.Click += new System.EventHandler(this.btnPageInfo_Click);
+			' 
+			' lblPageInfo
+			' 
+			Me.lblPageInfo.LineVisible = True
+			Me.lblPageInfo.Location = New System.Drawing.Point(165, 14)
+			Me.lblPageInfo.Name = "lblPageInfo"
+			Me.lblPageInfo.Size = New System.Drawing.Size(0, 13)
+			Me.lblPageInfo.TabIndex = 0
+			' 
+			' btnCheckLayout
+			' 
+			Me.btnCheckLayout.Location = New System.Drawing.Point(12, 45)
+			Me.btnCheckLayout.Name = "btnCheckLayout"
+			Me.btnCheckLayout.Size = New System.Drawing.Size(130, 23)
+			Me.btnCheckLayout.TabIndex = 3
+			Me.btnCheckLayout.Text = "Check Layout"
+'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
+'ORIGINAL LINE: this.btnCheckLayout.Click += new System.EventHandler(this.btnCheckLayout_Click);
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(784, 584)
+			Me.Controls.Add(Me.richEditControl1)
+			Me.Controls.Add(Me.panelControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+			DirectCast(Me.panelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.panelControl1.ResumeLayout(False)
+			Me.panelControl1.PerformLayout()
+			DirectCast(Me.trackBarControl1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+			DirectCast(Me.trackBarControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private richEditControl1 As DevExpress.XtraRichEdit.RichEditControl
+		Private panelControl1 As DevExpress.XtraEditors.PanelControl
+		Private lblPageInfo As DevExpress.XtraEditors.LabelControl
+		Private WithEvents btnPageInfo As DevExpress.XtraEditors.SimpleButton
+		Private WithEvents trackBarControl1 As DevExpress.XtraEditors.TrackBarControl
+		Private WithEvents btnCheckLayout As DevExpress.XtraEditors.SimpleButton
+	End Class
+End Namespace
+
