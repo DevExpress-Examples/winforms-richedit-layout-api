307aa25993150e752787e395eb95b5859cafb0dd
DevExpressExampleBot <fakeEmail>
2023-10-06 12:58:29 +0400

    Source auto update [skip ci]

--- VB/LayoutApiSimpleExample/Form1.vb
+++ VB/LayoutApiSimpleExample/Form1.vb
@@ -1,27 +1,28 @@
-﻿Imports DevExpress.XtraRichEdit.API.Layout
+Imports DevExpress.XtraRichEdit.API.Layout
 Imports System
 Imports System.Windows.Forms
 
 Namespace LayoutApiSimpleExample
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
         Public Sub New()
             InitializeComponent()
             richEditControl1.LoadDocument("Grimm.docx")
-            AddHandler richEditControl1.DocumentLayout.DocumentFormatted, AddressOf DocumentLayout_DocumentFormatted
+            AddHandler richEditControl1.DocumentLayout.DocumentFormatted, AddressOf Me.DocumentLayout_DocumentFormatted
         End Sub
-        #Region "#DocumentFormatted"
-        Private Sub DocumentLayout_DocumentFormatted(ByVal sender As Object, ByVal e As EventArgs)
 
+'#Region "#DocumentFormatted"
+        Private Sub DocumentLayout_DocumentFormatted(ByVal sender As Object, ByVal e As EventArgs)
             richEditControl1.BeginInvoke(New Action(Sub()
                 Dim pageCount As Integer = richEditControl1.DocumentLayout.GetFormattedPageCount()
                 For i As Integer = 0 To pageCount - 1
-                    Dim visitor As New MyDocumentLayoutVisitor()
+                    Dim visitor As MyDocumentLayoutVisitor = New MyDocumentLayoutVisitor()
                     visitor.Visit(richEditControl1.DocumentLayout.GetPage(i))
-                Next i
+                Next
             End Sub))
         End Sub
-        #End Region ' #DocumentFormatted
+'#End Region  ' #DocumentFormatted
     End Class
 End Namespace
